a0d327885ed2a3a09016ff5d7e9015155ab09727
Kartavya <kartavya.kothari24@gmail.com>
2020-04-26 00:37:42 +0530

    Downloading data files

---
+++ data/data-downlaod.sh
@@ -0,0 +1,7 @@
+wget www.cse.iitb.ac.in/~yashjain/copy_files/datasetXWikiWeb768.tsv
+wget www.cse.iitb.ac.in/~yashjain/copy_files/id2ClassMappingsXifengWikiWeb768.txt
+wget www.cse.iitb.ac.in/~yashjain/copy_files/dataset_GQ.tsv
+wget www.cse.iitb.ac.in/~yashjain/copy_files/dataset_TQ.tsv
+wget www.cse.iitb.ac.in/~yashjain/copy_files/dataset_qanta.tsv
+wget www.cse.iitb.ac.in/~yashjain/copy_files/dataset_SQ.tsv
+wget https://www.cse.iitb.ac.in/~kartavya/datafiles/glove_dict_tensor.pkl
